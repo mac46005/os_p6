cf51f05c24cbfaaeec42d73126690f359243b40e
Marco Preciado <mac46005@gmail.com>
2026-05-04 17:40:46 -0500

    msg manager implemeneted but will probably have to change sendMessage later. For now this is its implementation.

---
+++ include/msg/msg_manager.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <unistd.h>
+#include <functional>
+#include "../error/error.hpp"
+#include "msgbuffer.hpp"
+
+class MsgManager {
+    private:
+        int permission_ = 0;
+        int msqid_;
+        key_t key_;
+        pid_t pid_;
+    public:
+        explicit MsgManager(const char *key, int permission, pid_t pid);
+        // POSSIBLE CHANGE
+        void sendMessage(long mtype, pid_t sender_pid, ProcessStatus status, int resource, int message_flag);
+        void recieveMessage(std::function<void(MsgBuffer)> message, int message_flag);
+        void cleanUp();
+};
